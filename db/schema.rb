be39742248da06cdaaffcc16b25451861d660f5f
MatthewModica <mmodica29@gmail.com>
2023-07-15 11:51:25 -0400

    Rollback database to empty. Destroy courses model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_212546) do
-  create_table "courses", force: :cascade do |t|
-    t.string "campus"
-    t.string "term"
-    t.string "component"
-    t.integer "catalog_number"
-    t.string "instruction_mode"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
