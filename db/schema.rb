f9cc40265aa1f4bd8d5a7cf0ad5a6cb6c332e1d1
MatthewModica <mmodica29@gmail.com>
2023-07-15 12:01:24 -0400

    Generate User scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_155303) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_15_155926) do
   create_table "courses", primary_key: "course_number", id: :string, force: :cascade do |t|
     t.string "name"
     t.string "subject"
     t.text "description"
     t.string "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", primary_key: "email", id: :string, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "role"
+    t.boolean "is_approved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
