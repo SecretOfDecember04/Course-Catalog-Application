6735305d74f6e48a423e976038e228ce5f4decb8
MatthewModica <mmodica29@gmail.com>
2023-07-15 12:49:27 -0400

    Generate Recommendation scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_164529) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_15_164842) do
   create_table "course_takens", force: :cascade do |t|
     t.string "grader"
     t.string "course"
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_164529) do
     t.string "name"
     t.text "description"
     t.string "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recommendations", force: :cascade do |t|
+    t.string "instructor"
+    t.string "student"
+    t.string "section_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
