b370a008aa960509db6dcfac9d6adbe2208e22b4
Noah Hein <noahhein123@gmail.com>
2023-07-17 19:53:24 -0400

    Added migration to link the sections, enrollments, and users tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :course_takens, dependent: :destroy
+  has_many :enrollments, dependent: :destroy
   validates(:email, :first_name, :last_name, :role, :is_approved, presence: true)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_234216) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_235139) do
   create_table "applications", force: :cascade do |t|
     t.string "student"
     t.string "course"
@@ -91,4 +91,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_234216) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "enrollments", "sections"
+  add_foreign_key "enrollments", "users"
 end
