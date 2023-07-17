54e6f81870690229b50ff8890811fa846449294f
Noah Hein <noahhein123@gmail.com>
2023-07-17 19:42:35 -0400

    Created enrollment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_233836) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_234216) do
   create_table "applications", force: :cascade do |t|
     t.string "student"
     t.string "course"
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_233836) do
     t.string "name"
     t.text "description"
     t.string "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
