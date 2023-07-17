9136f89d3f503010f3aa416fd182864452670d42
Noah Hein <noahhein123@gmail.com>
2023-07-17 19:39:08 -0400

    Added devise to project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :course_takens, dependent: :destroy
   validates(:email, :first_name, :last_name, :role, :is_approved, presence: true)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_172114) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_233836) do
   create_table "applications", force: :cascade do |t|
     t.string "student"
     t.string "course"
@@ -68,7 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_172114) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.string "first_name"
     t.string "last_name"
     t.string "role"
@@ -76,6 +75,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_172114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "availabilities"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
