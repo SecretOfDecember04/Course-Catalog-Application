26caedf3f162e27dc615f63e0206b5913d7eda46
Noah Hein <noahhein123@gmail.com>
2023-07-23 17:21:11 -0400

    Trying to fix devise

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :sections
   resources :users
   resources :courses
-
-  # root to: "home#index"
+  authenticate :user do
+    root to: 'home#index', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
 end
