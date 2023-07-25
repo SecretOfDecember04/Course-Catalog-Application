4681eb4279754b9179e98a7282de42b5dfb59c5c
MatthewModica <mmodica29@gmail.com>
2023-07-25 19:15:35 -0400

    Update ApiService class

--- app/services/api_service.rb
+++ app/services/api_service.rb
@@ -1,8 +1,7 @@
 class ApiService
-  include HTTParty
   base uri('https://content.osu.edu/v2')
 
   def courses(search_params)
-    self.class.get('/classes/search', query: search_params)
+    HTTParty.get('/classes/search', query: search_params)
   end
 end
