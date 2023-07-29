97ab498e9f8313f883998d1dc13ef7a02828d71a
MatthewModica <mmodica29@gmail.com>
2023-07-29 13:21:52 -0400

    Add term filter to search bar

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,10 +3,16 @@ class CoursesController < ApplicationController
 
   # GET /courses or /courses.json
   def index
+    @terms = Course.distinct.pluck(:term)
+
     if params[:search].present?
       @pagy, @courses = pagy(
         Course.where("course_number LIKE :search OR subject LIKE :search OR name LIKE :search OR description LIKE :search OR level LIKE :search",
                      search: "%#{params[:search]}%")
+      )
+    elsif params[:term].present?
+      @pagy, @courses = pagy(
+        Course.where(term: params[:term])
       )
     else
       @pagy, @courses = pagy(Course.all)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  TERM_OPTIONS = {
+    '1232' => 'Spring 2023',
+    '1234' => 'Summer 2023',
+    '1238' => 'Autumn 2023'
+    # Add more term mappings as needed
+  }.freeze
+
+  def term_options_for_select
+    TERM_OPTIONS.map { |number, name| [name, number] }
+  end
 end
