973fa47e5222127e348eee97ca39898517837e32
Xianzhe Wang <wang.13599@osu.edu>
2023-08-01 18:08:55 -0400

    Edit seed for default admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 #Seed DB with at least 1 Admin User
-admin=User.create!({ name: "Admin User", email: 'admin.1@osu.edu',
-    password: 'password', password_confirmation: 'password'})
-
-admin.toggle!(:admin)
-
-if admin.valid?
-admin.save()
-
-elsif admin.errors.any?
-admin.errors.full_messages.each do |msg|
-puts msg
-end
-else
-puts "****NOT VALID****"
-end
-
 
 User.create(first_name: "Default", last_name: "Admin", 
     email: "admin.1@osu.edu", 
