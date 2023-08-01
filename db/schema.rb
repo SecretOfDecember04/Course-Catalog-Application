a1863be8de8d0d12968833506a3ef779d6a62c73
Noah Hein <noahhein123@gmail.com>
2023-08-01 17:28:06 -0400

    Set default value for is apporved in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_205122) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_01_212212) do
   create_table "applications", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -76,7 +76,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_205122) do
     t.string "first_name"
     t.string "last_name"
     t.string "role"
-    t.boolean "is_approved"
+    t.boolean "is_approved", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "availabilities"
