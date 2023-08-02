d47502d02f84fb19e45cc33227eed2d1335d178b
MatthewModica <mmodica29@gmail.com>
2023-08-01 22:27:13 -0400

    Fix section validation

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,5 @@
 class Section < ApplicationRecord
   has_many :enrollments
   belongs_to :course
-
+  validates :section_number, uniqueness: { scope: :term, message: "and term combination should be unique" }
 end
